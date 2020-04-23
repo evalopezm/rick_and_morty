2b05792030518908154ac5bc66604ccd88d0173c
evalopezm <evalopezm>
2020-04-23 09:57:01 +0200

    minor changes

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -6,7 +6,7 @@ function CharacterCard(props) {
 
   return (
     <li className='character-card'>
-      <img src={props.photo} />
+      <img className='character-photo' src={props.photo} alt={`${props.name}`} />
       <p>Name: {props.name}</p>
       <p>Species: {props.species}</p>
     </li>
