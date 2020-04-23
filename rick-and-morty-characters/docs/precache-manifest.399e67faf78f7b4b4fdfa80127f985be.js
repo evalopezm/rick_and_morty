ab0d726aabda8729b989e3debb6ea1af953c83e2
evalopezm <evalopezm>
2020-04-23 09:06:26 +0200

    Update docs

---
+++ rick-and-morty-characters/docs/precache-manifest.399e67faf78f7b4b4fdfa80127f985be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7619fc2ea575a10a78f58a6b7ca6aaa4",
+    "url": "./index.html"
+  },
+  {
+    "revision": "c953657c444ee6ed5181",
+    "url": "./static/css/main.24286269.chunk.css"
+  },
+  {
+    "revision": "ff2202c85273f03fa49c",
+    "url": "./static/js/2.93d71875.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.93d71875.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c953657c444ee6ed5181",
+    "url": "./static/js/main.2a1a41b7.chunk.js"
+  },
+  {
+    "revision": "3bb61d36ddfd8ec20639",
+    "url": "./static/js/runtime-main.eb1ab45c.js"
+  },
+  {
+    "revision": "75509f2ec9725b3cef07eefe651a4a8f",
+    "url": "./static/media/logo.75509f2e.png"
+  }
+]);
